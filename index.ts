9fdb6b7056673ebcebf5a3b9efdb9675a2a6b793
John <nnamanikester@gmail.com>
2022-05-25 15:25:17 +0100

    feat: added webhook

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
 import investmentBot from "./src/investmentBot";
+import express, { response } from "express";
+import axios from "axios";
+import { APP_NAME, BOT_TOKEN, WEBHOOK_URL } from "./src/config";
+
+const port = process.env.PORT || 3000;
+const app = express();
 
 investmentBot.launch();
 
 // Enable graceful stop
 process.once("SIGINT", () => investmentBot.stop("SIGINT"));
 process.once("SIGTERM", () => investmentBot.stop("SIGTERM"));
+
+app.get("/", async (req, res) => {
+  res.send("Active");
+});
+
+app.get("/webhook", async (req, res) => {
+  res.send("Hello World!");
+  const response = await axios.get(
+    `https://api.telegram.org/bot${BOT_TOKEN}/setWebhook?url=${WEBHOOK_URL}`
+  );
+  res.send(JSON.stringify(response.data));
+});
+
+app.listen(port, () => {
+  console.log(`${APP_NAME} listening on port ${port}`);
+});
+
+// https://api.telegram.org/bot<token>/setWebhook?url=https://<your_domain.com>/<path>
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,4 @@ export const BOT_TOKEN =
 export const GROUP_CHAT_ID = process.env.GROUP_CHAT_ID || "-1001179156055";
 export const APP_NAME = process.env.APP_NAME || "CoinbaseTrade";
 export const APP_URL = process.env.APP_URL || "https://oinbaseTrade.com";
+export const WEBHOOK_URL = process.env.WEBHOOK_URL || "";
