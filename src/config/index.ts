344741ba726d864326ce154159ccc41474904de4
kezieman <57093365+kezieman@users.noreply.github.com>
2022-05-30 09:56:02 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ export const BOT_TOKEN =
   process.env.BOT_TOKEN || "1717234638:AAFTxDjHyJWvlRxwcSO8ahiYgP2ecVTSshI";
 export const GROUP_CHAT_ID = process.env.GROUP_CHAT_ID || "-1001179156055";
 export const APP_NAME = process.env.APP_NAME || "CoinbaseTrade";
-export const APP_URL = process.env.APP_URL || "https://oinbaseTrade.com";
+export const APP_URL = process.env.APP_URL || "https://coinbaseTrade.com";
 export const WEBHOOK_URL = process.env.WEBHOOK_URL || "";
